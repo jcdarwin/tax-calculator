a5934d443439d734ffbae6f65bf691141057c34a
jdarwin <jcdarwin@gmail.com>
2025-10-04 23:03:18 +1300

    Fix bug in tax calculation

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,19 +31,19 @@ tax_brackets = [
     upper_cents: 15_600_00, # $15,600
     rate: 0.105
   },
-  { lower_cents: 15_601_00, # $15,601
+  { lower_cents: 15_600_00, # $15,600
     upper_cents: 53_500_00, # $53,500
     rate: 0.175
   },
-  { lower_cents: 53_501_00, # $53,501
+  { lower_cents: 53_500_00, # $53,500
     upper_cents: 78_100_00, # $78,100
     rate: 0.30
   },
-  { lower_cents: 78_101_00, # $78,101
+  { lower_cents: 78_100_00, # $78,100
     upper_cents: 180_000_00, # $180,000
     rate: 0.33
   },
-  { lower_cents: 180_001_00, # $180,001
+  { lower_cents: 180_000_00, # $180,000
     upper_cents: nil,
     rate: 0.39
   }
--- spec/requests/api/v1/tax_calculation_spec.rb
+++ spec/requests/api/v1/tax_calculation_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe 'GET /api/v1/tax_calculation', type: :request do
 
   # Current NZ tax brackets as at 4 October 2025
   # Refer: https://www.ird.govt.nz/income-tax/income-tax-for-individuals/tax-codes-and-tax-rates-for-individuals/tax-rates-for-individuals
-  let!(:bracket1) { TaxBracket.create!(currency: currency, lower_cents: 0,         upper_cents: 1_560_000, rate: BigDecimal('0.105')) }
-  let!(:bracket2) { TaxBracket.create!(currency: currency, lower_cents: 1_560_001, upper_cents: 5_350_000, rate: BigDecimal('0.175')) }
-  let!(:bracket3) { TaxBracket.create!(currency: currency, lower_cents: 5_350_001, upper_cents: 7_810_000, rate: BigDecimal('0.30')) }
-  let!(:bracket4) { TaxBracket.create!(currency: currency, lower_cents: 7_810_001, upper_cents: 18_000_000, rate: BigDecimal('0.33')) }
-  let!(:bracket5) { TaxBracket.create!(currency: currency, lower_cents: 18_000_001, upper_cents: nil,       rate: BigDecimal('0.39')) }
+  let!(:bracket1) { TaxBracket.create!(currency: currency, lower_cents: 0,          upper_cents: 1_560_000,  rate: BigDecimal('0.105')) }
+  let!(:bracket2) { TaxBracket.create!(currency: currency, lower_cents: 1_560_000,  upper_cents: 5_350_000,  rate: BigDecimal('0.175')) }
+  let!(:bracket3) { TaxBracket.create!(currency: currency, lower_cents: 5_350_000,  upper_cents: 7_810_000,  rate: BigDecimal('0.30')) }
+  let!(:bracket4) { TaxBracket.create!(currency: currency, lower_cents: 7_810_000,  upper_cents: 18_000_000, rate: BigDecimal('0.33')) }
+  let!(:bracket5) { TaxBracket.create!(currency: currency, lower_cents: 18_000_000, upper_cents: nil,        rate: BigDecimal('0.39')) }
 
   def json_body
     JSON.parse(response.body)
@@ -58,6 +58,49 @@ RSpec.describe 'GET /api/v1/tax_calculation', type: :request do
       # 15,600 * 10.5% = 1,638.00
       # $0.01 at 17.5% ~= 0.00175 -> rounds to 0.00
       expect(body['total_tax']).to eq('1638.00')
+    end
+
+    it 'returns correct tax for 10,000' do
+      get '/api/v1/tax_calculation', params: { income: 10000 }
+      expect(response).to have_http_status(:ok)
+      body = json_body
+      # 10,000 * 10.5% = 1,050.00
+      expect(body['total_tax']).to eq('1050.00')
+    end
+
+    it 'returns correct tax for 35,000' do
+      get '/api/v1/tax_calculation', params: { income: 35000 }
+      expect(response).to have_http_status(:ok)
+      body = json_body
+      # 15,600 * 10.5% = 1,638.00
+      # (35,000 - 15,600) * 17.5% = 3,395.00
+      # Total tax = 1,638 + 3,395.00 = 5,033.00
+      expect(body['total_tax']).to eq('5033.00')
+    end
+
+    it 'returns correct tax for 100,000' do
+      get '/api/v1/tax_calculation', params: { income: 100000 }
+      expect(response).to have_http_status(:ok)
+      body = json_body
+      # 15,600 * 10.5% = 1,638.00
+      # (53,500 - 15,600) * 17.5% = 6,632.50
+      # (78,100 - 53,500) * 30% = 7,380.00
+      # (100,000 - 78,100) * 33% = 7,227.00
+      # Total tax = 1,638 + 6,632.50 + 7,380 + 7,227 = 22,877.50
+      expect(body['total_tax']).to eq('22877.50')
+    end
+
+    it 'returns correct tax for 220,000' do
+      get '/api/v1/tax_calculation', params: { income: 220000 }
+      expect(response).to have_http_status(:ok)
+      body = json_body
+      # 15,600 * 10.5% = 1,638.00
+      # (53,500 - 15,600) * 17.5% = 6,632.50
+      # (78,100 - 53,500) * 30% = 7,380.00
+      # (180,000 - 78,100) * 33% = 34,377.00
+      # (220,000 - 180,000) * 39% = 15,600.00
+      # Total tax = 1,638 + 6,632.50 + 7,380 + 34,377 + 15,600 = 64,877.50
+      expect(body['total_tax']).to eq('64877.50')
     end
   end
 
